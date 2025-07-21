8a62a782df9ccba1af0f326223b09b21da9990aa
lunainwoozu <run2u7@gmail.com>
2025-07-21 14:46:45 +0900

    feat: 페이지 및 컴포넌트 1차 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.tsx
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'; // Link는 Header로 이동
+import './App.css';
+import Header from './components/Header'; // Header 컴포넌트 임포트
+import Home from './pages/Home';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Header는 모든 페이지에 표시될 것이므로 Routes 위에 배치합니다. */}
+      <Header />
+
+      {/* Routes는 앱 전체에서 한 번만 렌더링되어야 합니다. */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// import { useState } from 'react'
+
+const Header = () => {
+  // const [count, setCount] = useState(0)
+  return (
+    <nav style={{ marginBottom: '1rem', padding: '1rem'}}>
+      <Link to="/" style={{ marginRight: '1rem', textDecoration: 'none', color: 'blue' }} className='logo'>Home</Link>
+      {/* 필요한 다른 링크들을 여기에 추가하세요 */}
+    </nav>
+  )
+}
+
+export default Header
