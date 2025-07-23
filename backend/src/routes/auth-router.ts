7128ed716a47fffff956259915b674e5538a3b48
nsj <kyronares@naver.com>
2025-07-23 19:28:56 +0900

    feat:회원가입 스웨거 추가

--- backend/src/routes/auth-router.ts
+++ backend/src/routes/auth-router.ts
@@ -1,15 +1,45 @@
 import express, {Request,Response,Router} from 'express';
-const router:Router=express.Router()
+const router:Router=express.Router();
 import { AuthController } from '../controllers/auth-controller'
-const authController=new AuthController()
+const authController=new AuthController();
+import { LikesController } from '../controllers/likes-controller';
+const likesController=new LikesController();
 
-router.get('/register', (req:Request,res:Response)=>{
-  
-  authController.signUp('테스트5','테스트5','테스트5','테스트5')
-});
+
+/**
+ * @swagger
+ *
+ * /signup:
+ *  post:
+ *    summary: 회원 가입
+ *    description: "POST 방식으로 유저를 등록한다."
+ *    tags: [auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/x-www-form-urlencoded:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                description: "이름"
+ *              email:
+ *                type: string
+ *                description: "이메일"
+ *              password:
+ *                type: string
+ *                description: "비밀번호"
+ *              nickname:
+ *                type: string
+ *                description: "닉네임"
+ *      responses:
+ *        200:
+ *          description: 회원가입 성공
+ */
+router.post('/signup',authController.signUp);
 
 /*
-router.post('/signup', authController.register);
 router.post('/login', authController.login);
 router.post('/refresh', authController.refresh);
 router.post('/logout', authController.logout);
