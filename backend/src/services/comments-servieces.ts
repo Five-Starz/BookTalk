e2cc1715d184cc7d889d838fe3095d9816f45349
nsj <kyronares@naver.com>
2025-07-24 16:21:03 +0900

    feat: comment 스웨거 호환 및 아키텍쳐 수정

--- backend/src/services/comments-servieces.ts
+++ backend/src/services/comments-servieces.ts
@@ -4,23 +4,24 @@ const commentsRepository=new CommentsRepository();
 
 export class CommentsServices{
 
-  async findById(userId:number):Promise<Comments|null>{
-    return await commentsRepository.findById(userId)
+  async findById(userId:number):Promise<Comments[]>{
+    return await commentsRepository.findById(userId);
   };
   
   async creatComment(userId:number,reviewId:number,content:string,parentId:number|null):Promise<Comments>{
-    return await commentsRepository.creatComment(userId,reviewId,content,parentId)
+    return await commentsRepository.creatComment(userId,reviewId,content,parentId);
   };
 
-  async findByReviewId(reviewId:number){
-    await commentsRepository.findByReviewId(reviewId)
+  async findByReviewId(reviewId:number):Promise<Comments[]>{
+    console.log('ser')
+    return await commentsRepository.findByReviewId(reviewId);
   };
 
-  async updateComment(commentId:number,content:string){
-    await commentsRepository.updateComment(commentId,content)
+  async updateComment(commentId:number,content:string):Promise<Comments>{
+    return await commentsRepository.updateComment(commentId,content);
   };
 
-  async deleteComment(commentId:number){
-    await commentsRepository.deleteComment(commentId)
+  async deleteComment(commentId:number):Promise<boolean>{
+    return await commentsRepository.deleteComment(commentId);
   };
 };
