ad9022a2d66091b32a928a7ab5da410df3a0ed0c
Levv77 <oow_woo_@naver.com>
2025-07-28 17:17:32 +0900

    feat: login 기본 기능 구현 및 zustand 추가

--- frontend/src/components/ui/Form.tsx
+++ frontend/src/components/ui/Form.tsx
@@ -1,4 +1,13 @@
-import React from 'react'
+interface EmailFormProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+interface PasswordFormProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
 
 export const Form = () => {
   return (
@@ -21,25 +30,29 @@ export const NicknameForm = () => {
   )
 }
 
-export const EmailForm = () => {
+export const EmailForm = ({ value, onChange } :EmailFormProps) => {
   return (
     <div className="relative border-b">
       <label className="absolute top-3 left-4 text-gray-600 text-xs" htmlFor='email'>이메일</label>
       <input
         id='email'
         type="email"
+        value={ value }
+        onChange={ (e) => onChange(e.target.value) }
         className="w-full px-4 pt-8 pb-1 bg-white text-black rounded-md text-base outline-none border-none"
       />
     </div>
   )
 }
-export const PasswordForm = () => {
+export const PasswordForm = ({ value, onChange } :PasswordFormProps) => {
   return (
     <div className="relative border-b">
       <label className="absolute top-3 left-4 text-gray-600 text-xs" htmlFor='password'>비밀번호</label>
       <input
         id="password"
         type="password"
+        value={ value }
+        onChange={ (e) => onChange(e.target.value) }
         className="w-full px-4 pt-8 pb-1 bg-white text-black rounded-md text-base outline-none border-none"
       />
     </div>
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,18 +1,49 @@
+import axios from 'axios'
 import { LoginButton } from '../components/ui/Button'
 import { EmailForm, PasswordForm } from '../components/ui/Form'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const navigate = useNavigate();
+
+  const login = async (email: string, password: string) => {
+    try {
+      const res = await axios.post('http://localhost:8000/auth/login', {
+        email,
+        password,
+      },
+      {
+        withCredentials: true
+      }
+    );
+      console.log('Login Success', res.data);
+      navigate('/'); // 로그인 성공 후 홈으로 이동
+    } catch (error) {
+      console.log('Login Failed', error);
+    }
+  }
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    login(email, password);
+  }
+
   return (
     <>
       <div className="min-h-full pt-24 pb-24 flex flex-col justify-center items-center px-4">
         <h1 className="text-2xl font-bold  mb-8">로그인</h1>
         {/* 로그인 폼 */}
-        <form className="w-full max-w-md space-y-4">
+        <form className="w-full max-w-md space-y-4" onSubmit={ handleLogin }>
           {/* 이메일 */}
-          <EmailForm />
+          <EmailForm value={ email } onChange={ setEmail } />
 
           {/* 비밀번호 */}
-          <PasswordForm />
+          <PasswordForm value={ password } onChange={ setPassword }/>
 
           {/* 로그인 버튼 */}
           <LoginButton />
