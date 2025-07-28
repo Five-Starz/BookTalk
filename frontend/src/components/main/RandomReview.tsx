2c2682c68fcb9060b7bb3b22c015135245a64e8c
lunainwoozu <run2u7@gmail.com>
2025-07-28 13:03:45 +0900

    style: 메인 관련 컴포넌트 작업

--- frontend/src/components/main/RandomReview.tsx
+++ frontend/src/components/main/RandomReview.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import User from '../ui/User'
 
 const RandomReview = () => {
   return (
-    <div className='lg:w-2/5 rounded-lg p-4 bg-[#D8D5AF]'>
+    <div className='flex flex-col w-full rounded-lg p-4 gap-10 bg-[#D8D5AF] lg:w-2/5 lg:justify-between'>
       <h2>오늘의 랜덤 리뷰</h2>
-      <p>datadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatat</p>
+      <p className='text-overflow px-4'>datadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatatdatadatatdadatat</p>
+      <div className='flex gap-4 justify-center'>
+        <User width='6' />
+      </div>
     </div>
   )
 }
