5c28769df090dfc40241b78b22f440c67bfc8d39
Doi Kim <doyclub@naver.com>
2025-08-01 15:58:36 +0900

    Update book-controller.ts

--- backend/src/controllers/book-controller.ts
+++ backend/src/controllers/book-controller.ts
@@ -1,4 +1,3 @@
-import { par } from './../../node_modules/effect/src/internal/blockedRequests';
 // [역할] req, res 처리 담당. 클라이언트 응답 전용
 
 import { Request, Response } from 'express';
@@ -22,13 +21,13 @@ class BookController {
   }
 
   // 2. 도서 별 평균평점 조회
-  async getAverageRatingByBook(req:Request, res:Response) {
+  async getAverageRatingByBook(req:Request, res:Response): Promise<any> {
     try {
-      const {isbn} = req.params;  // 검색할 도서 ISBN 저장
+      const { isbn } = req.params;  // 검색할 도서 ISBN 저장
       console.log('[Book Controller] 받은 isbn', isbn);
 
       if (!isbn || typeof isbn !== 'string') {
-        return res.status(400).json({ message: '검색할 책 ISBN(query)을 입력해주세요. '})
+        return res.status(400).json({ message: '검색할 책 ISBN(params)을 입력해주세요. '})
       }
       const rating = await bookService.getAverageRatingByBook(isbn);  // ISBN으로 책 검색 후 평균평점 계산해 받아와
       console.log('[Book Controller] 서비스에서 받은 평균 평점: ', rating);
