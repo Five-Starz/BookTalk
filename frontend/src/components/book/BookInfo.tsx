5fa70fb3205b8a574bbde45d5e75a82efa9bca95
Levv77 <oow_woo_@naver.com>
2025-08-05 15:51:19 +0900

    Refactor: 평점이 있을 때 없을때 아이콘 색상 변경

--- frontend/src/components/book/BookInfo.tsx
+++ frontend/src/components/book/BookInfo.tsx
@@ -108,7 +108,15 @@ const BookInfo = ({ book }: { book: BookDetail }) => {
               보고싶어요 : { bookmarkCount }
             </button>
             <div className="flex flex-col w-1/3 p-4 sm:w-[150px] items-center gap-2 cursor-pointer">
-              <svg xmlns="http://www.w3.org/2000/svg" className='size-6 sm:size-8 lucide lucide-star-icon lucide-star' viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`size-6 sm:size-8 lucide lucide-star-icon lucide-star ${book.total_rating > 0 ? 'text-yellow-400' : 'text-gray-400'}`}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
               평점 : {book.total_rating >0 ? book.total_rating : '(리뷰부족)'}
             </div>
             <Link to={`/reviews/${finalIsbn}`} className="flex flex-col w-1/3 p-4 sm:w-[150px] items-center gap-2 cursor-pointer">
