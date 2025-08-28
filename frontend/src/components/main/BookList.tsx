67b7bf7d56c20ce3a268f4a5bc45c181456fa41a
run2u7@gmail.com <run2u7@gmail.com>
2025-08-28 18:02:15 +0900

    fix: useEffect 삭제

--- frontend/src/components/main/BookList.tsx
+++ frontend/src/components/main/BookList.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState, useRef} from 'react';
 import { Link } from 'react-router-dom';
 import { Swiper as SwiperClass } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
