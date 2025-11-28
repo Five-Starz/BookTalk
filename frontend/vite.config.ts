4d38a4d2f4d797e0b59d3b61f3ae672a75b05c7d
run2u7@gmail.com <run2u7@gmail.com>
2025-11-28 17:08:13 +0900

    fix: vite 프록시 설정 url 변경

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,32 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   // Vite 기준 프록시 설정
   server: {
     proxy: {
       '/auth': {
-        target: 'http://35.216.79.174:3000', // 백엔드 서버 주소로 변경
+        target: 'https://booktalk-server.onrender.com', // 백엔드 서버 주소로 변경
         changeOrigin: true,
         secure: false,
       },
       '/reviews': {
-        target: 'http://35.216.79.174:3000',
+        target: 'https://booktalk-server.onrender.com',
         changeOrigin: true,
         secure: false,
       },
       '/bookmarks': {
-        target: 'http://35.216.79.174:3000',
+        target: 'https://booktalk-server.onrender.com',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
